5ea7155d5343e518d93883dcb5c035aaa5d2de05
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-09 19:11:08 +0000

    Automatically backed up by Learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,16 +1,16 @@
 export default function createStore(reducer) {
   // add your code here
     let state;
-   
+
     function dispatch(action) {
       state = reducer(state, action);
       render();
     }
-   
+
     function getState() {
       return state;
     };
-   
+
     return {
       dispatch,
       getState
@@ -24,27 +24,27 @@ function render() {
 
 function createStore(reducer) {
 };
- 
+
 function changeCount(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
- 
+
     default:
       return state;
   }
 }
- 
- 
+
+
 function render() {
   let container = document.getElementById('container');
   container.textContent = store.getState().count;
 };
- 
+
 let store = createStore(changeCount) // createStore takes the changeCount reducer as an argument
 store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
- 
+
 button.addEventListener('click', function() {
   store.dispatch({ type: 'INCREASE_COUNT' });
 });
